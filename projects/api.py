9b2274dc781857f6a6d09f042cab293d05e2f353
xavi <softwarenexosdev2@gmail.com>
2023-10-30 21:09:08 -0500

    first commit

---
+++ projects/api.py
@@ -0,0 +1,28 @@
+from .models import Project
+from rest_framework import viewsets, permissions
+from .serializers import ProjectSerializer
+class ProjectViewSet(viewsets.ModelViewSet):
+    
+    queryset = Project.objects.all()
+    permission_classes = [ permissions.AllowAny ]
+    serializer_class = ProjectSerializer
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    queryset = Project.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = ProjectSerializer
